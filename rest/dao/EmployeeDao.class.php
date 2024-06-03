46150e424c35500ccd4e76063081fefde550abf5
vedran-kilalic <mckosarka@gmail.com>
2024-06-03 22:07:57 +0200

    Full project

--- rest/dao/EmployeeDao.class.php
+++ rest/dao/EmployeeDao.class.php
@@ -10,16 +10,38 @@ class EmployeeDao extends BaseDao {
     return $this->get_all();
 }
 
-
 public function get_employee_by_id($id){
     return $this->get_by_id($id);
 }
-
   function get_employee_by_name($name)
   {
     return $this->query_unique("SELECT * FROM employees WHERE name = :name", ["name" =>$name]);
     
   }
+  
+  public function get_employee_by_email($email) {
+    $query = "SELECT *
+              FROM employees
+              WHERE email = :email";
+    return $this->query_unique($query, ['email' => $email]);
+}
+
+  public function check_password($employee_id, $password){
+    $query = "SELECT *
+              FROM employees
+              WHERE id = :id
+              AND password=:password";
+    $result = $this->query_unique($query, ['id' => $employee_id, 'password' => $password]);
+    if ($result){
+      return true;
+    } else{
+      return false;
+    }
+  }
+  public function get_employee_id($employee_id) {
+    $query = "SELECT * FROM employees WHERE id = :id";
+    return $this->query_unique($query, ['id' => $employee_id]);
+}
 
 }
 
