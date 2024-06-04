e710e4cefb3de1980a4f43bf5e2ae5f8f7cbd3cc
vedran-kilalic <mckosarka@gmail.com>
2024-06-04 02:19:56 +0200

    revert changes

--- rest/swagger/swagger.php
+++ rest/swagger/swagger.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost:80/Web-Employee-Management-System/rest');
 } else {
-    define('BASE_URL', 'https://seashell-app-mh569.ondigitalocean.app//Web-Employee-Management-System/');
+    define('BASE_URL', 'https://urchin-app-sr3f6.ondigitalocean.app/Web-Employee-Management-System/');
 }
 
 error_reporting(0);
